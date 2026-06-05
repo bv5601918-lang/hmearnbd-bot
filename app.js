84eaab47fe094d0b688ddecb85431a713f719661
bv5601918-lang <bv5601918@gmail.com>
2026-06-05 12:27:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-let balance = 0;
-
-document.querySelector(".btn").addEventListener("click", function () {
-    balance += 0.50;
-    document.getElementById("balance").innerText = "৳" + balance.toFixed(2);
-});
 function showPage(page) {
+
     document.getElementById("home").style.display = "none";
     document.getElementById("tasks").style.display = "none";
     document.getElementById("packages").style.display = "none";
